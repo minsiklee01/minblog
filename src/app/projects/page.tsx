f4cf3a6593835fa586ca552dd65ef339902abe0f
bwutee <leeminsik999@gmail.com>
2025-06-22 17:46:02 -0600

    fix not a module

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,11 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 export default function Page() {
   return(
     <div>
       <h1>Projects Home</h1>
       <p>
-        <Link href={``}>Project1</Link>
-        <Link href={``}>Project2</Link>
+        {/* <Link href={``}>Project1</Link>
+        <Link href={``}>Project2</Link> */}
       </p>
     </div>
 
